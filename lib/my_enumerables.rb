5460bd560ede8bde2d2a7dd799f66ba950b0a408
onyxvillamor <awsgegud420@gmail.com>
2025-12-26 07:57:34 +0800

    first commit

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,102 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+
+    index = 0
+    my_each do |elem|
+      yield(elem, index)
+      index += 1
+    end
+
+    self
+  end
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
+
+    result = []
+    my_each do |elem|
+      result << elem if yield(elem)
+    end
+    result
+  end
+
+  def my_all?
+    my_each do |elem|
+      return false unless yield(elem)
+    end
+    true
+  end
+
+  def my_any?
+    my_each do |elem|
+      return true if yield(elem)
+    end
+    false
+  end
+
+  def my_none?
+    my_each do |elem|
+      return false if yield(elem)
+    end
+    true
+  end
+
+  def my_count(item = nil)
+    count = 0
+
+    if block_given?
+      my_each { |elem| count += 1 if yield(elem) }
+    elsif !item.nil?
+      my_each { |elem| count += 1 if elem == item }
+    else
+      my_each { count += 1 }
+    end
+
+    count
+  end
+
+  def my_map(proc = nil)
+    result = []
+
+    if proc
+      my_each { |elem| result << proc.call(elem) }
+    elsif block_given?
+      my_each { |elem| result << yield(elem) }
+    else
+      return to_enum(:my_map)
+    end
+
+    result
+  end
+
+  def my_inject(initial = nil)
+    accumulator = initial
+    index = 0
+
+    my_each do |elem|
+      if accumulator.nil? && index == 0
+        accumulator = elem
+      else
+        accumulator = yield(accumulator, elem)
+      end
+      index += 1
+    end
+
+    accumulator
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
 class Array
-  # Define my_each here
+  def my_each
+    return to_enum(:my_each) unless block_given?
+
+    i = 0
+    while i < length
+      yield(self[i])
+      i += 1
+    end
+
+    self
+  end
 end
